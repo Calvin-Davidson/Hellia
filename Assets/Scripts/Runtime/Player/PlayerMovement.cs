17ef1602b153aee3165699f9891a57dc48d07f02
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-05-12 11:44:51 +0200

    Movement fixes and cameraController
    Added a camera controller that rotates around the player, added a system to the playerMovement script that makes the camera follow the player.
    
    the PlayerMovement now moves relative to the camera.

--- Assets/Scripts/Runtime/Player/PlayerMovement.cs
+++ Assets/Scripts/Runtime/Player/PlayerMovement.cs
@@ -5,9 +5,12 @@ namespace Runtime.Player
     [RequireComponent(typeof(CharacterController))]
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField] private  CharacterController controller;
+        [SerializeField] private CharacterController controller;
         [SerializeField] private float gravity = 9.81f;
         [SerializeField] private float playerSpeed = 5.0f;
+
+        [SerializeField] private bool relativeToCamera;
+        [SerializeField] private Transform cameraTransform;
 
         private Vector3 _playerVelocity;
         private bool _isGrounded;
@@ -20,16 +23,37 @@ namespace Runtime.Player
                 _playerVelocity.y = 0f;
             }
 
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            controller.Move(move * (Time.deltaTime * playerSpeed));
+            if (!relativeToCamera) return;
 
-            if (move != Vector3.zero)
+            Vector3 prevCamPosition = cameraTransform.position;
+            Vector3 playerPosition = transform.position;
+
+            float horizontalAxis = Input.GetAxis("Horizontal");
+            float verticalAxis = Input.GetAxis("Vertical");
+
+            var forward = cameraTransform.forward;
+            var right = cameraTransform.right;
+
+            forward.y = 0f;
+            right.y = 0f;
+            forward.Normalize();
+            right.Normalize();
+
+            var desiredMoveDirection = forward * verticalAxis + right * horizontalAxis;
+
+
+            controller.Move(desiredMoveDirection * (Time.deltaTime * playerSpeed));
+
+            if (desiredMoveDirection != Vector3.zero)
             {
-                gameObject.transform.forward = move;
+                gameObject.transform.forward = desiredMoveDirection;
             }
 
             _playerVelocity.y += -gravity * Time.deltaTime;
             controller.Move(_playerVelocity * Time.deltaTime);
+
+            Vector3 positionDifference = playerPosition - transform.position;
+            cameraTransform.position = prevCamPosition - positionDifference;
         }
     }
 }
