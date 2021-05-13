26f8f76c36986bc47c39ce7148c7cb5112314846
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-05-13 12:33:26 +0200

    You have to look at the object you wanna push now.

--- Assets/Scripts/Runtime/Movables/MovableBlock.cs
+++ Assets/Scripts/Runtime/Movables/MovableBlock.cs
@@ -8,15 +8,60 @@ namespace Runtime.Movables
     {
         [SerializeField] private Vector3 blockSize;
         [SerializeField] private float cornerTolerance;
-        
         [SerializeField] private LayerMask pushableByLayers;
+        [SerializeField] private bool shouldObjectLookAtMe = false;
 
 
-        public void TryPushTo(Vector3 direction)
+        private static readonly float RotationTolerance = 20;
+        private static readonly int MoveDistance = 4;
+
+        public void TryPushTo(Vector3 direction, GameObject collidedObject)
         {
             int colliders = Physics.OverlapBoxNonAlloc(transform.position + direction, blockSize, new Collider[1]);
-            if (colliders == 0)
+            if (colliders == 0 && !shouldObjectLookAtMe)
+            {
                 transform.position += direction;
+            }
+            else if (colliders == 0 && IsLookingAtMe(collidedObject, direction))
+            {
+                transform.position += direction;
+            }
+        }
+
+        private bool IsLookingAtMe(GameObject other, Vector3 pushDirection)
+        {
+            Vector3 colliderRotation = other.transform.rotation.eulerAngles;
+            if (pushDirection.x >= MoveDistance &&
+                IsBetweenRotation(colliderRotation.y, 90 - RotationTolerance, 90 + RotationTolerance))
+            {
+                return true;
+            }
+
+            if (pushDirection.x <= -MoveDistance &&
+                IsBetweenRotation(colliderRotation.y, 270 - RotationTolerance, 270 + RotationTolerance))
+            {
+                return true;
+            }
+
+            if (pushDirection.z >= MoveDistance &&
+                IsBetweenRotation(colliderRotation.y, 360 - RotationTolerance, 360 + RotationTolerance))
+            {
+                return true;
+            }
+
+            if (pushDirection.z <= -MoveDistance &&
+                IsBetweenRotation(colliderRotation.y, 180 - RotationTolerance, 180 + RotationTolerance))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsBetweenRotation(float value, float min, float max)
+        {
+            float fullRotationFix = Math.Abs(value - 360);
+            return (value > min && value < max || fullRotationFix > min && fullRotationFix < max);
         }
 
         private void OnCollisionStay(Collision other)
@@ -36,16 +81,16 @@ namespace Runtime.Movables
                 if (math.abs(xDifference) > math.abs(zDifference))
                 {
                     if (contactDirection.x < 0)
-                        TryPushTo(new Vector3(-4, 0, 0));
+                        TryPushTo(new Vector3(-MoveDistance, 0, 0), other.gameObject);
                     else
-                        TryPushTo(new Vector3(4, 0, 0));
+                        TryPushTo(new Vector3(MoveDistance, 0, 0), other.gameObject);
                 }
                 else
                 {
                     if (contactDirection.z < 0)
-                        TryPushTo(new Vector3(0, 0, -4));
+                        TryPushTo(new Vector3(0, 0, -MoveDistance), other.gameObject);
                     else
-                        TryPushTo(new Vector3(0, 0, 4));
+                        TryPushTo(new Vector3(0, 0, MoveDistance), other.gameObject);
                 }
             }
         }
