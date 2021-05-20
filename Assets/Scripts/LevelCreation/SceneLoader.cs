bb0fb9dea6bbad18430a35b5d765c8fb03752d35
Ingmar-van-Busschbach <TheCatPlaysGamez@gmail.com>
2021-05-20 12:16:05 +0200

    Updated Scene Loader script

--- Assets/Scripts/LevelCreation/SceneLoader.cs
+++ Assets/Scripts/LevelCreation/SceneLoader.cs
@@ -5,12 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] private string inputKey = "space";
     [SerializeField] private string sceneName;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown(inputKey))
         {
             SceneManager.LoadScene(sceneName);
         }
