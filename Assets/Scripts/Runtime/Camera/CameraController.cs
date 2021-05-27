3b061c95198053722c72690146f13c9aa8beae2d
Ingmar-van-Busschbach <TheCatPlaysGamez@gmail.com>
2021-05-27 11:13:18 +0200

    Reworked camera controller

--- Assets/Scripts/Runtime/Camera/CameraController.cs
+++ Assets/Scripts/Runtime/Camera/CameraController.cs
@@ -9,7 +9,6 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private float rotationSpeed;
 
-
     private void Start()
     {
         PlayerMovement.PlayerMoveEvent.AddListener(OnPlayerMove);
@@ -23,11 +22,32 @@ public class CameraController : MonoBehaviour
 
     private void LateUpdate()
     {
+        Vector3 offset = transform.position - target.position;
+        float offsetLength = offset.magnitude;
+        Vector3 axis = offset;
+
+        axis.y = 0;
+        axis = Quaternion.Euler(0, -90, 0) * axis;
+        axis.Normalize();
+        offset.Normalize();
+
+        transform.position += offset * -Input.GetAxis("Mouse ScrollWheel");
         float horizontal = Input.GetAxis("Mouse X");
+        float vertical = -Input.GetAxis("Mouse Y");
+        float angle = transform.rotation.eulerAngles.x;
+
+        if (angle > 60)
+        {
+            vertical = Mathf.Clamp(vertical, -10f, 0f);
+        }
+        if (angle < 30)
+        {
+            vertical = Mathf.Clamp(vertical, 0f, 10f);
+        }
 
         transform.LookAt(target);
-
         transform.RotateAround (target.position,new Vector3(0.0f,1.0f,0.0f),horizontal * Time.deltaTime * rotationSpeed);
-
+        transform.RotateAround(target.position, axis, vertical * Time.deltaTime * rotationSpeed);
+        
     }
 }
