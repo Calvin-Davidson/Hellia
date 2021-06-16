6a8927b8d0a7b12bb431d3cddc35c9dbff90699d
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-06-16 10:04:38 +0200

    Added a dialog system to display text in the tutorial levels

---
+++ Assets/Scripts/Runtime/dialog/DialogData.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct DialogData
+{
+    public String message;
+    public float displayFor;
+}
